e5779825baa3bc1a1a91676721e2663760d17be1
Lucas Bravo <bplucca@gmail.com>
2023-07-10 16:29:52 -0400

    arreglos Webpay codigos

--- routes.js
+++ routes.js
@@ -2,14 +2,17 @@
 const express = require('express');
 const router = express.Router();
 const { WebpayPlus } = require('transbank-sdk');
+const { Options, IntegrationApiKeys, Environment, IntegrationCommerceCodes } = require("transbank-sdk"); // CommonJS
 
 router.post('/pago', async (req, res) => {
   try {
     // Obtén los datos necesarios de la solicitud
     const { buyOrder, sessionId, amount, returnUrl } = req.body;
 
+    //WebpayPlus.configureForProduction('597055555530', '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C');
+
     // Crea una transacción utilizando el SDK de Transbank
-    const createResponse = await (new WebpayPlus.Transaction()).create(
+    const createResponse = await (new WebpayPlus.Transaction(new Options(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY, Environment.Integration))).create(
         buyOrder, 
         sessionId, 
         amount, 
